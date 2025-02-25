2fb2ba29bab456ece0c4d67b708d542d3e2bcee9
Fantomas42 <fantomas42@gmail.com>
2025-02-25 10:27:51 +0100

    Add csTimer import script

---
+++ term_timer/scripts/cs_timer.py
@@ -0,0 +1,58 @@
+import sys
+from argparse import ArgumentParser
+from datetime import datetime
+from pathlib import Path
+
+from term_timer.constants import SECOND
+
+
+def date_to_ns(date: str) -> int:
+    date_format = '%Y-%m-%d %H:%M:%S'
+    dt = datetime.strptime(date, date_format)  # noqa: DTZ007
+
+    timestamp_seconds = dt.timestamp()
+
+    return int(timestamp_seconds * SECOND)
+
+
+def time_to_ns(time: str) -> int:
+    minutes_str = '0'
+    reste = time
+    if ':' in time:
+        minutes_str, reste = time.split(':')
+
+    seconds_str, centiseconds_str = reste.split('.')
+
+    minutes = int(minutes_str)
+    seconds = int(seconds_str)
+    centiseconds = int(centiseconds_str)
+
+    total_seconds = minutes * 60 + seconds + centiseconds / 100
+
+    return int(total_seconds * SECOND)
+
+
+def import_csv() -> None:
+    parser = ArgumentParser(
+        description='Import csTimer solves in CSV',
+    )
+    parser.add_argument(
+        'export',
+        help='csTimer csv file to import',
+    )
+
+    options = parser.parse_args(sys.argv[1:])
+
+    export_path = Path(options.export)
+
+    with export_path.open() as fd:
+        lines = fd.readlines()
+
+    for line in lines[1:]:
+        (_i, time, _, scramble, date, __) = line.split(';')
+        start_date = date_to_ns(date)
+        end_date = start_date + time_to_ns(time)
+
+        print(
+            ';'.join([time, str(start_date), str(end_date), scramble, '', '']),
+        )
