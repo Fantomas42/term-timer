18159dffeb708c42c79ff99667bfd2344af1e685
Fantomas42 <fantomas42@gmail.com>
2025-03-29 22:55:02 +0100

    Better interface

--- term_timer/bluetooth/interface.py
+++ term_timer/bluetooth/interface.py
@@ -1,4 +1,3 @@
-import asyncio
 import logging
 
 from bleak import BleakClient
@@ -27,6 +26,72 @@ class BluetoothInterface:
 
     def __init__(self, queue):
         self.queue = queue
+
+    async def __aenter__(self) -> bool:
+        device = await self.scan()
+
+        if not device:
+            logger.warning(
+                'No bluetooth cube found.\n'
+                "Make sure it's powered on and in pairing mode.",
+            )
+            raise CubeNotFoundError
+
+        self.device = device
+        self.client = BleakClient(self.device.address)
+        await self.client.connect()
+
+        logger.info(' * Connected: %r', self.client.is_connected)
+
+        for service in self.client.services:
+            for driver in DRIVERS:
+                if service.uuid == driver.service_uid:
+                    logger.info(' * Using %s driver', driver.__name__)
+                    self.driver = driver(self.client, self.device)
+                    break
+            if self.driver:
+                break
+
+        if not self.driver:
+            logger.warning('No driver found')
+
+        await self.client.start_notify(
+            self.driver.state_characteristic_uid,
+            self.notification_handler,
+        )
+
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, exc_traceback):
+        await self.client.stop_notify(
+            self.driver.state_characteristic_uid,
+        )
+        await self.client.disconnect()
+        # Send an "exit command to the consumer"
+        await self.queue.put(None)
+
+    async def notification_handler(self, sender, data):
+        events = await self.driver.event_handler(sender, data)
+        await self.queue.put(events)
+
+    async def send_command(self, command: str) -> bool:
+        """Send a command to the cube"""
+        if not self.client or not self.client.is_connected:
+            logger.warning('Not connected to cube')
+            return False
+
+        msg = self.driver.send_command_handler(command)
+
+        if not msg:
+            logger.warning('Unknown command')
+            return False
+
+        await self.client.write_gatt_char(
+            self.driver.command_characteristic_uid,
+            msg,
+        )
+
+        return True
 
     async def scan(self):
         logger.info(
@@ -57,73 +122,3 @@ class BluetoothInterface:
             return None
 
         return selected_device
-
-    async def scan_and_connect(self) -> bool:
-        device = await self.scan()
-
-        if not device:
-            logger.warning(
-                'No bluetooth cube found.\n'
-                "Make sure it's powered on and in pairing mode.",
-            )
-            return CubeNotFoundError
-
-        self.device = device
-
-        async with BleakClient(self.device.address) as self.client:
-            logger.info(' * Connected: %r', self.client.is_connected)
-
-            for service in self.client.services:
-                for driver in DRIVERS:
-                    if service.uuid == driver.service_uid:
-                        logger.info(' * Using %s driver', driver.__name__)
-                        self.driver = driver(self.client, self.device)
-                        break
-                if self.driver:
-                    break
-
-            if not self.driver:
-                logger.warning('No driver found')
-
-            # Subscribe to notifications for state changes
-            await self.client.start_notify(
-                self.driver.state_characteristic_uid,
-                self.notification_handler,
-            )
-
-            # Initialize/reset the cube
-            await self.send_command('REQUEST_HARDWARE')
-            await self.send_command('REQUEST_FACELETS')
-            await self.send_command('REQUEST_BATTERY')
-
-        return True
-
-    async def notification_handler(self, sender, data):
-        events = await self.driver.event_handler(sender, data)
-        await self.queue.put(events)
-
-    async def send_command(self, command: str) -> bool:
-        """Send a command to the cube"""
-        if not self.client or not self.client.is_connected:
-            logger.warning('Not connected to cube')
-            return False
-
-        msg = self.driver.send_command_handler(command)
-
-        if not msg:
-            logger.warning('Unknown command')
-            return False
-
-        await self.client.write_gatt_char(
-            self.driver.command_characteristic_uid,
-            msg,
-        )
-
-        return True
-
-    async def disconnect(self):
-        await self.client.stop_notify(
-            self.driver.state_characteristic_uid,
-        )
-        # Send an "exit command to the consumer"
-        await self.queue.put(None)
--- term_timer/scripts/bluetooth_info.py
+++ term_timer/scripts/bluetooth_info.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 
 async def consumer_cb(queue):
-    logger.info('CONSUMER : Start consumming')
+    logger.info('CONSUMER: Start consumming')
 
     internal_cube = None
 
@@ -19,12 +19,11 @@ async def consumer_cb(queue):
         console.print(str(cube), end='')
 
     while True:
-        # Use await asyncio.wait_for(queue.get(), timeout=1.0)
-        # if you want a timeout for getting data.
         events = await queue.get()
+
         if events is None:
             logger.info(
-                'CONSUMER : Got message from client about disconnection. '
+                'CONSUMER: Got message from client about disconnection. '
                 'Exiting consumer loop...',
             )
             break
@@ -33,7 +32,7 @@ async def consumer_cb(queue):
             event_name = event['event']
             if event_name == 'hardware':
                 logger.info(
-                    'CONSUMER : Hardware %s version %s, Software %s, %s',
+                    'CONSUMER: Hardware %s version %s, Software %s, %s',
                     event['hardware_name'],
                     event['hardware_version'],
                     event['software_version'],
@@ -44,37 +43,41 @@ async def consumer_cb(queue):
                 )
             elif event_name == 'battery':
                 logger.info(
-                    'CONSUMER : Battery : %s%%',
+                    'CONSUMER: Battery: %s%%',
                     event['level'],
                 )
             elif event_name == 'facelets':
                 internal_cube = Cube(3, event['facelets'])
                 logger.info(
-                    'CONSUMER : Facelets initialized',
+                    'CONSUMER: Facelets initialized',
                 )
                 show_cube(internal_cube)
 
             elif event_name == 'move':
-                if internal_cube:
-                    internal_cube.rotate([event['move']])
-                    show_cube(internal_cube)
                 logger.info(
-                    'CONSUMER : Move : Face : %s, Direction : %s, Move : %s',
+                    'CONSUMER: Face: %s, Direction: %s, Move: %s',
                     event['face'],
                     event['direction'],
                     event['move'],
                 )
+                if internal_cube:
+                    internal_cube.rotate([event['move']])
+                    show_cube(internal_cube)
 
 
 async def client_cb(queue):
-    cube = BluetoothInterface(queue)
 
-    await cube.scan_and_connect()
-    logger.info('Free play for 5 seconds')
-    await asyncio.sleep(5.0)
-    await cube.disconnect()
+    async with BluetoothInterface(queue) as bi:
 
-    logger.warning('Disconnected')
+        # Initialize/reset the cube
+        await bi.send_command('REQUEST_HARDWARE')
+        await bi.send_command('REQUEST_BATTERY')
+        await bi.send_command('REQUEST_FACELETS')
+
+        logger.info('Free play for 10s')
+        await asyncio.sleep(10.0)
+
+    logger.warning('Interface disconnected')
 
 
 async def run():
