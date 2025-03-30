7c3e95b89a3fb6b1133eff5d97c75be3effad8ed
Fantomas42 <fantomas42@gmail.com>
2025-03-30 09:12:43 +0200

    Better bt timer

---
+++ term_timer/scripts/bluetooth_timer.py
@@ -0,0 +1,85 @@
+import asyncio
+import logging
+from random import seed
+
+from term_timer.arguments import get_arguments
+from term_timer.console import console
+from term_timer.in_out import load_solves
+from term_timer.in_out import save_solves
+from term_timer.stats import StatisticsReporter
+from term_timer.timer import Timer
+
+
+async def timer() -> int:  # noqa: PLR0912
+    options = get_arguments()
+
+    logging.disable(logging.INFO)
+
+    cube = options.cube
+    if options.stats or options.list or options.graph:
+        session_stats = StatisticsReporter(cube, load_solves(cube))
+
+        if options.list:
+            session_stats.listing(options.list)
+
+        if options.stats:
+            session_stats.resume('Global ', show_title=True)
+
+        if options.graph:
+            session_stats.graph()
+
+        return 0
+
+    free_play = options.free_play
+    if options.seed or options.iterations or options.easy_cross:
+        free_play = True
+
+    if free_play:
+        stack = []
+        console.print(
+            ':lock: Mode Free Play is active, solves will not be recorded !',
+            style='warning',
+        )
+    else:
+        stack = load_solves(cube)
+
+    if options.seed:
+        seed(options.seed)
+
+    solves_done = 0
+
+    while 42:
+        timer = Timer(
+            cube_size=cube,
+            iterations=options.iterations,
+            easy_cross=options.easy_cross,
+            free_play=free_play,
+            show_cube=options.show_cube,
+            countdown=options.countdown,
+            metronome=options.metronome,
+            stack=stack,
+        )
+
+        done = await timer.start()
+        stack = timer.stack
+
+        if done:
+            solves_done += 1
+
+            if options.solves and solves_done >= options.solves:
+                break
+        else:
+            break
+
+    if not free_play:
+        save_solves(cube, stack)
+
+    if len(stack) > 1:
+        session_stats = StatisticsReporter(cube, stack)
+        session_stats.resume((free_play and 'Session ') or 'Global ')
+
+    return 0
+
+
+def main() -> int:
+    return asyncio.run(timer())
