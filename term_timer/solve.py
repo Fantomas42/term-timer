1810e35f7084766624bc59f62269413e86142745
Fantomas42 <fantomas42@gmail.com>
2025-04-22 20:57:25 +0200

    Handle ratio

--- term_timer/solve.py
+++ term_timer/solve.py
@@ -167,8 +167,13 @@ class Solve:
 
                 footer = ''
                 if info['type'] != 'virtual':
+                    ratio_1 = info['inspection'] / info['total'] * 13
+                    ratio_2 = info['execution'] / info['total'] * 13
+
                     footer += (
-                        '\n               '
+                        '\n'
+                        f'[inspection]{ round(ratio_1) * " " }[/inspection]'
+                        f'[execution]{ round(ratio_2) * " " }[/execution] '
                         f'[consign]{ self.missed_moves_line(info["reconstruction"]) }[/consign]'
                     )
 
