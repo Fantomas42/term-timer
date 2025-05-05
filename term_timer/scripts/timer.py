92037d3822c941564086af23918572e2f0335b93
Fantomas42 <fantomas42@gmail.com>
2025-05-05 20:59:22 +0200

    Fix double Ctrl+C issue on long async tasks

--- term_timer/scripts/timer.py
+++ term_timer/scripts/timer.py
@@ -14,57 +14,8 @@ from term_timer.stats import StatisticsReporter
 from term_timer.timer import Timer
 
 
-async def timer() -> int:
-    configure_logging()
-
-    options = get_arguments()
-    command = COMMAND_RESOLUTIONS.get(options.command, options.command)
-
-    if command == 'import':
-        Importer().import_file(options.source)
-
-        return 0
-
+async def timer(options) -> int:
     cube = options.cube
-
-    if command in {'list', 'stats', 'graph', 'cfop', 'detail'}:
-        session_stats = StatisticsReporter(
-            cube,
-            load_all_solves(
-                cube,
-                options.include_sessions,
-                options.exclude_sessions,
-                options.devices,
-            ),
-        )
-
-        if not session_stats.stack:
-            console.print(
-                f'No saved solves yet for { session_stats.cube_name }.',
-                style='warning',
-            )
-            return 1
-
-        if command == 'list':
-            session_stats.listing(options.count, options.sort)
-
-        if command == 'stats':
-            session_stats.resume('Global ', show_title=True)
-
-        if command == 'cfop':
-            session_stats.cfop(
-                options.oll, options.pll,
-                options.sort, options.order,
-            )
-
-        if command == 'graph':
-            session_stats.graph()
-
-        if command == 'detail':
-            for solve_id in options.solves:
-                session_stats.detail(solve_id, options.method)
-
-        return 0
 
     free_play = options.free_play
     if options.seed or options.iterations or options.easy_cross:
@@ -114,6 +65,59 @@ async def timer() -> int:
     return 0
 
 
+def tools(command, options):
+    cube = options.cube
+
+    session_stats = StatisticsReporter(
+        cube,
+        load_all_solves(
+            cube,
+            options.include_sessions,
+            options.exclude_sessions,
+            options.devices,
+        ),
+    )
+
+    if not session_stats.stack:
+        console.print(
+            f'No saved solves yet for { session_stats.cube_name }.',
+            style='warning',
+        )
+        return 1
+
+    if command == 'list':
+        session_stats.listing(options.count, options.sort)
+
+    if command == 'stats':
+        session_stats.resume('Global ', show_title=True)
+
+    if command == 'cfop':
+        session_stats.cfop(
+            options.oll, options.pll,
+            options.sort, options.order,
+        )
+
+    if command == 'graph':
+        session_stats.graph()
+
+    if command == 'detail':
+        for solve_id in options.solves:
+            session_stats.detail(solve_id, options.method)
+
+    return 0
+
+
 def main() -> int:
+    configure_logging()
+
+    options = get_arguments()
+    command = COMMAND_RESOLUTIONS.get(options.command, options.command)
+
     with suppress(KeyboardInterrupt):
-        return asyncio.run(timer(), debug=DEBUG)
+        if command == 'solve':
+            return asyncio.run(timer(options), debug=DEBUG)
+        if command == 'import':
+            Importer().import_file(options.source)
+            return 0
+        tools(command, options)
+        return 0
