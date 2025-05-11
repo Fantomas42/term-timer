a76a9a365d98875dcefa5d5e4b8b19ee781facda
Fantomas42 <fantomas42@gmail.com>
2025-05-11 21:52:50 +0200

    Skeleton driver

--- term_timer/bluetooth/drivers/moyu.py
+++ term_timer/bluetooth/drivers/moyu.py
@@ -2,6 +2,10 @@
 References :
   - https://github.com/lukeburong/weilong-v10-ai-protocol
 """
+import logging
+import time
+from datetime import datetime
+from datetime import timezone
 
 from term_timer.bluetooth.constants import MOYU_WEILONG_COMMAND_CHARACTERISTIC
 from term_timer.bluetooth.constants import MOYU_WEILONG_ENCRYPTION_KEY
@@ -9,7 +13,11 @@ from term_timer.bluetooth.constants import MOYU_WEILONG_SERVICE
 from term_timer.bluetooth.constants import MOYU_WEILONG_STATE_CHARACTERISTIC
 from term_timer.bluetooth.drivers.base import Driver
 from term_timer.bluetooth.encrypter import GanGen2CubeEncrypter
+from term_timer.bluetooth.facelets import to_kociemba_facelets
+from term_timer.bluetooth.message import GanProtocolMessage
 from term_timer.bluetooth.salt import get_salt
+
+logger = logging.getLogger(__name__)
 
 
 class MoyuWeilong10Driver(Driver):
@@ -20,6 +28,10 @@ class MoyuWeilong10Driver(Driver):
     state_characteristic_uid = MOYU_WEILONG_STATE_CHARACTERISTIC
     command_characteristic_uid = MOYU_WEILONG_COMMAND_CHARACTERISTIC
     encrypter = GanGen2CubeEncrypter
+
+    last_serial = -1
+    cube_timestamp = 0
+    last_move_timestamp = 0
 
     def init_cypher(self):
         return self.encrypter(
@@ -54,3 +66,169 @@ class MoyuWeilong10Driver(Driver):
             return False
 
         return self.cypher.encrypt(bytes(msg))
+
+    async def event_handler(self, sender, data):  # noqa: ARG002
+        """Process notifications from the cube"""
+        clock = time.perf_counter_ns()
+        timestamp = datetime.now(tz=timezone.utc)  # noqa: UP017
+
+        events = []
+
+        msg = GanProtocolMessage(
+            self.cypher.decrypt(data),
+        )
+        event = msg.get_bit_word(0, 8)
+
+        if event == 0xAB:  # Gyroscope
+            if self.disable_gyro:
+                return []
+
+        #     # Orientation Quaternion
+        #     qw = msg.get_bit_word(4, 16)
+        #     qx = msg.get_bit_word(20, 16)
+        #     qy = msg.get_bit_word(36, 16)
+        #     qz = msg.get_bit_word(52, 16)
+
+        #     # Angular Velocity
+        #     vx = msg.get_bit_word(68, 4)
+        #     vy = msg.get_bit_word(72, 4)
+        #     vz = msg.get_bit_word(76, 4)
+
+        #     payload = {
+        #         'event': 'gyro',
+        #         'clock': clock,
+        #         'timestamp': timestamp,
+        #         'quaternion': {
+        #             'x': (1 - (qx >> 15) * 2) * (qx & 0x7FFF) / 0x7FFF,
+        #             'y': (1 - (qy >> 15) * 2) * (qy & 0x7FFF) / 0x7FFF,
+        #             'z': (1 - (qz >> 15) * 2) * (qz & 0x7FFF) / 0x7FFF,
+        #             'w': (1 - (qw >> 15) * 2) * (qw & 0x7FFF) / 0x7FFF,
+        #         },
+        #         'velocity': {
+        #             'x': (1 - (vx >> 3) * 2) * (vx & 0x7),
+        #             'y': (1 - (vy >> 3) * 2) * (vy & 0x7),
+        #             'z': (1 - (vz >> 3) * 2) * (vz & 0x7),
+        #         },
+        #     }
+
+        #     self.add_event(events, payload)
+
+        elif event == 0xA5:  # Moves
+            if self.last_serial == -1:  # Block moves until facelets received
+                return []
+
+            serial = msg.get_bit_word(88, 8)
+            diff = min((serial - self.last_serial) & 0xFF, 7)
+
+            self.last_serial = serial
+
+            if diff <= 0:
+                return []
+
+            # for i in range(diff - 1, -1, -1):
+            #     face = msg.get_bit_word(12 + 5 * i, 4)
+            #     direction = msg.get_bit_word(16 + 5 * i, 1)
+            #     move = 'URFDLB'[face] + " '"[direction]
+            #     elapsed = msg.get_bit_word(47 + 16 * i, 16)
+
+            #     # In case of 16-bit cube timestamp register overflow
+            #     if elapsed == 0:
+            #         elapsed = timestamp - self.last_move_timestamp
+
+            #     self.cube_timestamp += elapsed
+            #     payload = {
+            #         'event': 'move',
+            #         'clock': clock,
+            #         'timestamp': timestamp,
+            #         'serial': (serial - i) & 0xFF,
+            #         # Missed and recovered events
+            #         # has no meaningful local timestamps
+            #         'local_timestamp': timestamp if i == 0 else None,
+            #         'cube_timestamp': self.cube_timestamp,
+            #         'face': face,
+            #         'direction': direction,
+            #         'move': move.strip(),
+            #     }
+            #     self.add_event(events, payload)
+
+            self.last_move_timestamp = timestamp
+
+        elif event == 0xA3:  # Facelets
+            serial = msg.get_bit_word(152, 8)
+
+            if self.last_serial == -1:
+                self.last_serial = serial
+
+            # # Corner/Edge Permutation/Orientation
+            # cp = []
+            # co = []
+            # ep = []
+            # eo = []
+            # # Corners
+            # for i in range(7):
+            #     cp.append(msg.get_bit_word(12 + i * 3, 3))
+            #     co.append(msg.get_bit_word(33 + i * 2, 2))
+            # cp.append(28 - sum(cp))
+            # co.append((3 - (sum(co) % 3)) % 3)
+            # # Edges
+            # for i in range(11):
+            #     ep.append(msg.get_bit_word(47 + i * 4, 4))
+            #     eo.append(msg.get_bit_word(91 + i, 1))
+            # ep.append(66 - sum(ep))
+            # eo.append((2 - (sum(eo) % 2)) % 2)
+
+            # payload = {
+            #     'event': 'facelets',
+            #     'clock': clock,
+            #     'timestamp': timestamp,
+            #     'serial': serial,
+            #     'facelets': to_kociemba_facelets(cp, co, ep, eo),
+            #     'state': {
+            #         'CP': cp,
+            #         'CO': co,
+            #         'EP': ep,
+            #         'EO': eo,
+            #     },
+            # }
+            # self.add_event(events, payload)
+
+        elif event == 0xA1:  # Hardware
+            hw_major = msg.get_bit_word(72, 8)
+            hw_minor = msg.get_bit_word(80, 8)
+            sw_major = msg.get_bit_word(88, 8)
+            sw_minor = msg.get_bit_word(96, 8)
+            gyro_enabled = msg.get_bit_word(105, 1)
+            gyro_supported = msg.get_bit_word(106, 1)
+            logger.info('GYRO ENABLED %s', gyro_enabled)
+            logger.info('GYRO SUPPORTED %s', gyro_supported)
+            serial = msg.get_bit_word(109, 8)
+
+            hardware_name = msg.get_bit_word(8, 64)
+
+            payload = {
+                'event': 'hardware',
+                'clock': clock,
+                'timestamp': timestamp,
+                'hardware_name': hardware_name,
+                'hardware_version': f'{ hw_major }.{ hw_minor }',
+                'software_version': f'{ sw_major }.{ sw_minor }',
+                'gyroscope_supported': (
+                    bool(gyro_supported)
+                    and bool(gyro_enabled)
+                ),
+                'serial': serial
+            }
+            self.add_event(events, payload)
+
+        elif event == 0xA4:  # Battery
+            battery_level = msg.get_bit_word(8, 8)
+
+            payload = {
+                'event': 'battery',
+                'clock': clock,
+                'timestamp': timestamp,
+                'level': min(battery_level, 100),
+            }
+            self.add_event(events, payload)
+
+        return events
