bf900671a2184210a3f035e5440685179df5f716
Fantomas42 <fantomas42@gmail.com>
2025-05-30 16:16:26 +0200

    Prepare trainer

--- term_timer/trainer.py
+++ term_timer/trainer.py
@@ -1,6 +1,10 @@
+from term_timer.constants import DNF
+from term_timer.constants import MS_TO_NS_FACTOR
+from term_timer.formatter import format_time
 from term_timer.interface import SolveInterface
 from term_timer.scrambler import scramble_moves
 from term_timer.scrambler import trainer
+from term_timer.solve import Solve
 
 
 class Trainer(SolveInterface):
@@ -16,13 +20,14 @@ class Trainer(SolveInterface):
 
         self.counter = 1
 
-    def start_line(self, cube) -> None:
+    def start_line(self, cube, case) -> None:
         if self.show_cube:
             self.console.print(getattr(cube, self.mode)(), end='')
 
         self.console.print(
             f'[scramble]Training #{ self.counter }:[/scramble]',
             f'[moves]{ self.scramble }[/moves]',
+            f'[comment]// { case }[/comment]',
         )
 
         if self.bluetooth_interface:
@@ -38,10 +43,33 @@ class Trainer(SolveInterface):
                 end='', style='consign',
             )
 
+    def solve_line(self, solve: Solve) -> None:
+        self.clear_line(full=True)
+
+        if solve.raw_moves:
+            if solve.flag != DNF:
+                self.console.print(solve.method_line, end='')
+                self.console.print(
+                    f'[analysis]Analysis #{ self.counter }:[/analysis] '
+                    f'{ solve.report_line }',
+                )
+            else:
+                self.console.print(
+                    f'[duration]Duration #{ self.counter }:[/duration]',
+                    f'[result]{ format_time(self.elapsed_time) }[/result]',
+                    '[dnf]DNF[/dnf]',
+                )
+                return
+
+        self.console.print(
+            f'[duration]Duration #{ self.counter }:[/duration]',
+            f'[result]{ format_time(self.elapsed_time) }[/result]',
+        )
+
     async def start(self) -> bool:
         self.init_solve()
 
-        self.scramble, cube = trainer(self.mode)
+        case, self.scramble, cube = trainer(self.mode)
 
         if self.bluetooth_cube and not self.bluetooth_cube.is_solved:
             scramble = scramble_moves(
@@ -49,9 +77,10 @@ class Trainer(SolveInterface):
                 self.bluetooth_cube.state,
             )
             self.scramble = scramble
+        self.scramble_oriented = self.reorient(self.scramble)
         self.facelets_scrambled = cube.get_kociemba_facelet_positions()
 
-        self.start_line(cube)
+        self.start_line(cube, case)
 
         quit_solve = await self.scramble_solve()
 
@@ -63,8 +92,32 @@ class Trainer(SolveInterface):
 
         self.elapsed_time = self.end_time - self.start_time
 
-        self.counter += 1
+        flag = ''
+        moves = []
+        if self.moves:
+            if not self.bluetooth_cube.is_solved:
+                flag = DNF
 
-        # self.solve_line()
+            first_time = self.moves[0]['time']
+            for move in self.moves:
+                timing = int((move['time'] - first_time) / MS_TO_NS_FACTOR)
+                moves.append(f'{ move["move"] }@{ timing }')
+
+        solve = Solve(
+            self.date,
+            self.elapsed_time,
+            str(self.scramble),
+            flag=flag,
+            timer='Term-Timer',
+            device=(
+                self.bluetooth_interface
+                and self.bluetooth_interface.device.name
+            ) or '',
+            moves=' '.join(moves),
+        )
+
+        self.solve_line(solve)
+
+        self.counter += 1
 
         return True
