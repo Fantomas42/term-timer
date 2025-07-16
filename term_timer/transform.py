51abf235e7ffaee8e2ed94bf7339d83065207fcb
Fantomas42 <fantomas42@gmail.com>
2025-07-16 23:27:04 +0200

    Improve humanize moves

--- term_timer/transform.py
+++ term_timer/transform.py
@@ -2,6 +2,7 @@ from cubing_algs.algorithm import Algorithm
 from cubing_algs.transform.degrip import degrip_full_moves
 from cubing_algs.transform.fat import refat_moves
 from cubing_algs.transform.optimize import optimize_double_moves
+from cubing_algs.transform.rotation import compress_final_rotations
 from cubing_algs.transform.rotation import remove_final_rotations
 from cubing_algs.transform.slice import reslice_moves
 from cubing_algs.transform.timing import untime_moves
@@ -20,13 +21,18 @@ def reorient_moves(orientation: Algorithm, algorithm: Algorithm) -> Algorithm:
 
 def humanize_moves(algorithm: Algorithm) -> Algorithm:
     # Note this will work until orientation move are implemented
-    return algorithm.transform(
+    humanized = algorithm.transform(
         reslice_moves,
         degrip_full_moves,
         refat_moves,
-        remove_final_rotations,
+        compress_final_rotations,
         to_fixpoint=True,
     )
+
+    if humanized[-1].is_rotation_move:
+        return algorithm
+
+    return humanized
 
 
 def prettify_moves(algorithm: Algorithm) -> Algorithm:
