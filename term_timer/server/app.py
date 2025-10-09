bb7b652f5a77666794056dcbeacb11710ef7006a
Fantomas42 <fantomas42@gmail.com>
2025-10-09 22:55:53 +0200

    Y axe variations

--- term_timer/server/app.py
+++ term_timer/server/app.py
@@ -20,6 +20,9 @@ from bottle import redirect
 from bottle import request
 from bottle import static_file
 from cubing_algs.algorithm import Algorithm
+from cubing_algs.transform.offset import offset_y_moves
+from cubing_algs.transform.offset import offset_y2_moves
+from cubing_algs.transform.offset import offset_yprime_moves
 from cubing_algs.transform.optimize import optimize_double_moves
 from cubing_algs.transform.pause import pause_moves
 from cubing_algs.transform.size import compress_moves
@@ -711,8 +714,25 @@ class AlgorithmDetailView(View):
         self.algorithm = Algorithm.parse_moves(algorithm)
 
     def get_context(self) -> dict[str, Any]:
+        # Generate Y-axis variations
+        y_variations = [
+            {
+                'label': "y'",
+                'algorithm': offset_y_moves(self.algorithm),
+            },
+            {
+                'label': 'y2',
+                'algorithm': offset_y2_moves(self.algorithm),
+            },
+            {
+                'label': 'y',
+                'algorithm': offset_yprime_moves(self.algorithm),
+            },
+        ]
+
         return {
             'algorithm': self.algorithm,
+            'y_variations': y_variations,
         }
 
 
