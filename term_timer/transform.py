d5bcd17c3e9cf6bb018d78165b04f1675f5f45bd
Fantomas42 <fantomas42@gmail.com>
2025-10-18 21:42:46 +0200

    Update humanize_moves

--- term_timer/transform.py
+++ term_timer/transform.py
@@ -1,37 +1,19 @@
 from cubing_algs.algorithm import Algorithm
-from cubing_algs.transform.degrip import degrip_full_moves
 from cubing_algs.transform.optimize import optimize_double_moves
-from cubing_algs.transform.rotation import compress_final_rotations
 from cubing_algs.transform.slice import reslice_timed_moves
 from cubing_algs.transform.timing import untime_moves
 from cubing_algs.transform.translate import translate_pov_moves
-from cubing_algs.transform.wide import rewide_moves
 from cubing_algs.transform.wide import rewide_timed_moves
 
 from term_timer.constants import RESLICE_THRESHOLD
+from term_timer.constants import REWIDE_THRESHOLD
 
 
 def humanize_moves(algorithm: Algorithm) -> Algorithm:
-    # Note: this will work until orientation move are implemented
-    humanized = algorithm.transform(
-        reslice_timed_moves(RESLICE_THRESHOLD),
-        degrip_full_moves,
-        rewide_moves,
-        compress_final_rotations,
-        to_fixpoint=True,
-    )
-
-    if humanized and humanized[-1].is_rotation_move:
-        return algorithm
-
-    return humanized
-
-
-def humanize_moves_new(algorithm: Algorithm) -> Algorithm:
     return algorithm.transform(
         translate_pov_moves,
-        reslice_timed_moves(120, (3,)),
-        rewide_timed_moves(50),
+        reslice_timed_moves(RESLICE_THRESHOLD, (3,)),
+        rewide_timed_moves(REWIDE_THRESHOLD),
     )
 
 
