dee657b1e17e80b3a1fa177fe4593bcbbdce6c81
Fantomas42 <fantomas42@gmail.com>
2026-01-02 00:14:49 +0100

    More tests for special f2l and xcross solves

---
+++ term_timer/tests/test_solve_f2l.py
@@ -0,0 +1,104 @@
+"""Tests for solve with F2L special cases."""
+import unittest
+from typing import cast
+
+from term_timer.methods.base import Analyser
+from term_timer.solve import Solve
+
+
+def get_method_applied(solve: Solve) -> Analyser:
+    """
+    Get method_applied, asserting it's not None in tests.
+
+    Returns:
+        The method_applied Analyser instance.
+
+    """
+    return cast('Analyser', solve.method_applied)
+
+
+class TestSolveOLLSkip(unittest.TestCase):
+    """Test Solve with OLL skip for checking output."""
+
+    maxDiff = None
+
+    def setUp(self) -> None:
+        """Test setup."""
+        self.date = 1728144170
+        self.time = 47158000000
+        self.scramble = """
+        L' B2 R' D2 L2 D2 U2 F2 R' B2 L D U' B' R F U2 B R U' F'
+        """
+        self.solution = """
+        R'@0 D@255 L@691 L'@1243 U'@1690 L@1960 R@2673 R@2748 F@3175 F@3477
+        D@5732 D@5986 D@7329 R'@7859 D'@8284 R@8633 D'@8740 D'@8972 R'@9060
+        D@9616 D'@10037 D'@10279 F'@10670 D'@10853 F@11190 D@12405 D@13759
+        B@14401 D'@14536 B'@15234 D@15439 B@15556 D'@16676 D'@17099 B@17367
+        D'@17458 B'@17637 D'@17762 D@18796 D@19658 B@20007 B@20077 B@21044
+        D'@21139 B'@21489 D@21655 B@21778 B'@22469 D'@23929 B'@24657 D@24846
+        B@25241 D'@26359 D'@26567 F@26869 D'@26956 F'@27056 D'@27209 B@29579
+        R'@29774 B'@29936 R'@31050 D@31285 R@31640 D'@31934 D'@32138 R'@32436
+        D@32617 D@32875 R@33191 D'@33403 R'@33703 D@33850 R@34189 L'@37124
+        D@37263 L@37372 D@37529 B@38033 D'@38152 B'@38303 D'@40143 D'@42083
+        D'@42290 B'@43187 L@43380 B@43552 L'@43846 D'@44273 L'@44636 D@45200
+        L@45589 D@46525 D'@46938 D'@47158
+        """
+
+        self.solve = Solve(
+            self.date,
+            self.time,
+            self.scramble,
+            moves=self.solution,
+        )
+
+        self.solve.method_name = 'cf4op'
+        self.solve.orientation = 'auto'
+
+    def test_summary(self) -> None:
+        """Test summary."""
+        method_applied = get_method_applied(self.solve)
+        inputs = method_applied.summary
+        outputs = [
+            ('Cross', 'step'),
+            ('F2L', 'virtual'),
+            ('F2L 1+2', 'substep'),
+            ('F2L 3', 'substep'),
+            ('F2L 4', 'substep'),
+            ('OLL', 'step'),
+            ('PLL', 'step'),
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                source['name'],
+                expected[0],
+            )
+            self.assertEqual(
+                source['type'],
+                expected[1],
+            )
+
+    def test_reconstruction_step_text(self) -> None:
+        """Test reconstruction step text."""
+        method_applied = get_method_applied(self.solve)
+        inputs = [
+            info
+            for info in method_applied.summary
+            if info['type'] != 'virtual'
+        ]
+        outputs = [
+            "R' U L L' D' L R2 B2 .",
+            "U2 . U R' U' R U2 R' U U2 B' U' B . U . U F U' F' U F . "
+            "U2 F U' F' U' . U . U F2 . F U' F' U F F' . U' F' U F . "
+            "U2 B U' B' U' . F R' F' .",
+            "R' U R U2 R' U2 R U' R' U R .",
+            "L' U L U F U' F' .",
+            "U' . U2 . F' L F L' U' L' U L .",
+            'U U2',
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                self.solve.reconstruction_step_text(source, multiple=False),
+                expected,
+            )
---
+++ term_timer/tests/test_solve_xcross.py
@@ -0,0 +1,189 @@
+"""Tests for solve with XCross."""
+import unittest
+from typing import cast
+
+from term_timer.methods.base import Analyser
+from term_timer.solve import Solve
+
+
+def get_method_applied(solve: Solve) -> Analyser:
+    """
+    Get method_applied, asserting it's not None in tests.
+
+    Returns:
+        The method_applied Analyser instance.
+
+    """
+    return cast('Analyser', solve.method_applied)
+
+
+class TestSolveXCross(unittest.TestCase):
+    """Test Solve with XCross for checking output."""
+
+    maxDiff = None
+
+    def setUp(self) -> None:
+        """Test setup."""
+        self.date = 1739130391
+        self.time = 31932000000
+        self.scramble = """
+        D L2 D' B2 L2 B2 U2 B2 D R2 U R2 B' U' R2 B2 L' B' F D2 L
+        """
+        self.solution = """
+        F'@0 U@283 B'@853 B'@917 F'@4320 F'@4399 R@4576 F@4743 U@5484 D@8402
+        B@8609 D'@8918 B'@9117 D@9369 D@9625 F'@9907 D@10124 F@10251 D'@12240
+        F@12592 D@12735 F'@12810 D@13114 D@13356 F@13525 D'@13590 F'@13789
+        D'@14152 D'@14377 L@15020 L@15111 L'@15195 D@15514 L@15635 L@16320
+        D'@16422 L'@16523 D'@17057 D'@17295 B@18042 D'@18147 B'@18230 D@18475
+        B'@18766 D'@18931 B@19001 B@20649 D@20804 D@21099 B'@21248 D'@21383
+        D'@21614 B@21713 D@21796 B'@21848 D'@22890 B@23295 D'@23442 D'@23653
+        B@23873 B@23927 D'@24427 B@24763 B@24842 D'@25374 B@26109 B@26182
+        D'@26365 D'@26611 B@26802 D'@28094 D'@29985 B@30322 D@30415 B'@30491
+        L'@30747 B@30912 D@31033 B'@31100 D'@31214 B'@31402 L@31486 B@31638
+        B@31702 D'@31766 B'@31932
+        """
+
+        self.solve = Solve(
+            self.date,
+            self.time,
+            self.scramble,
+            moves=self.solution,
+        )
+
+        self.solve.method_name = 'cf4op'
+        self.solve.orientation = 'auto'
+
+    def test_summary(self) -> None:
+        """Test summary."""
+        method_applied = get_method_applied(self.solve)
+        inputs = method_applied.summary
+        outputs = [
+            ('XCross', 'step'),
+            ('F2L', 'virtual'),
+            ('F2L 2', 'substep'),
+            ('F2L 3', 'substep'),
+            ('F2L 4', 'substep'),
+            ('OLL', 'step'),
+            ('PLL', 'step'),
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                source['name'],
+                expected[0],
+            )
+            self.assertEqual(
+                source['type'],
+                expected[1],
+            )
+
+    def test_reconstruction_step_text(self) -> None:
+        """Test reconstruction step text."""
+        method_applied = get_method_applied(self.solve)
+        inputs = [
+            info
+            for info in method_applied.summary
+            if info['type'] != 'virtual'
+        ]
+        outputs = [
+            "L' D R2 . L2 B L . D . U R U' R' U2 L' U L . "
+            "U' L U L' U2 L U' L' U2 . F2",
+            "F' U F . F U' F'",
+            "U2 . R U' R' U R' U' R .",
+            "R U2 R' U2 R U R' .",
+            "U' R U2 R2 U' R2 U' . R2 U2 R .",
+            "U' . U' R U R' F' R U R' U' R' F R2 U' R'",
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                self.solve.reconstruction_step_text(source, multiple=False),
+                expected,
+            )
+
+
+class TestSolveXXCross(unittest.TestCase):
+    """Test Solve with XXCross for checking output."""
+
+    maxDiff = None
+
+    def setUp(self) -> None:
+        """Test setup."""
+
+        self.date = 1713907485
+        self.time = 41206000000
+        self.scramble = """
+  U' B L2 F L F U D B R B2 L2 B2 R' F2 L' F2 L' B2 L'
+        """
+        self.solution = """
+        B@0 U'@327 L@1295 D'@2015 F@2334 F@2424 U@3583 D'@4719 D@7229 F'@7825
+        D@7953 D@8249 F@8459 D'@8689 F'@8841 D@8909 F@9092 D@10498 B@10893
+        D'@11160 D'@11409 B'@11552 D@11740 B@11868 D'@11951 B'@12121 R@12934
+        D@13026 R'@13107 D'@13218 D@14248 R@14732 D'@14954 D'@15197 R'@15365
+        D@15513 R@15633 D'@15720 R'@15869 F@17120 D@17223 F'@17352 D'@17485
+        F'@18117 R@18285 F@18516 D@19568 R@19722 D'@19829 R'@19948 D'@20884
+        D@22189 R'@22481 D@22726 D@23033 R@23383 D'@23629 D'@23883 R'@24348
+        D@24689 R@24982 D'@25714 F@26211 L@26406 D@26556 L'@26601 D'@26718
+        F'@27357 D@28376 L@28971 L@29052 U@29307 L'@29494 D'@29687 D'@29937
+        L@30217 U'@30569 L'@30676 D'@30810 D'@31021 L'@31178 R@35307 L'@35310
+        L'@35588 R@35588 D@36351 B@36961 F'@36967 B@37286 F'@37287 U'@37576
+        F'@38266 B@38301 D@38781 U'@38785 U'@39132 D@39141 B@39784 F'@39919
+        D@40944 D@41206
+        """
+
+        self.solve = Solve(
+            self.date,
+            self.time,
+            self.scramble,
+            moves=self.solution,
+        )
+
+        self.solve.method_name = 'cf4op'
+        self.solve.orientation = 'auto'
+
+    def test_summary(self) -> None:
+        """Test summary."""
+        method_applied = get_method_applied(self.solve)
+        inputs = method_applied.summary
+        outputs = [
+            ('XXCross', 'step'),
+            ('F2L', 'virtual'),
+            ('F2L 3', 'substep'),
+            ('F2L 4', 'substep'),
+            ('OLL', 'step'),
+            ('PLL', 'step'),
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                source['name'],
+                expected[0],
+            )
+            self.assertEqual(
+                source['type'],
+                expected[1],
+            )
+
+    def test_reconstruction_step_text(self) -> None:
+        """Test reconstruction step text."""
+        method_applied = get_method_applied(self.solve)
+        inputs = [
+            info
+            for info in method_applied.summary
+            if info['type'] != 'virtual'
+        ]
+        outputs = [
+            "L D' . B . U' R2 . D . U' . U R' U2 R U' R' U R . "
+            "U L U2 L' U L U' L' . F",
+            "U F' U' . U F U2 F' U F U' F' . R U R' U' R' F R . "
+            "U F U' F' .",
+            "U' . U F' U2 F U2 F' U F .",
+            "U' R B U B' U' R' . U B2 D B' U2 B D' B' U2 B' .",
+            "F B2 F . U L R' L R' D' R' L U D2 U L R' . U2",
+        ]
+
+        for source, expected in zip(inputs, outputs, strict=True):
+            self.assertEqual(
+                self.solve.reconstruction_step_text(source, multiple=False),
+                expected,
+            )
