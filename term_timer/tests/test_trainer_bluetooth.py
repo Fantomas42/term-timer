4aa53f5520fd6d916ae32c528f559582931b608f
Fantomas42 <fantomas42@gmail.com>
2026-06-10 17:02:43 +0200

    Try to fix CI

--- term_timer/tests/test_trainer_bluetooth.py
+++ term_timer/tests/test_trainer_bluetooth.py
@@ -324,7 +324,7 @@ class SaveTrainingsPatchedTestCase(unittest.IsolatedAsyncioTestCase):
         patcher.start()
         self.addCleanup(patcher.stop)
 
-        sound_patcher = patch('term_timer.interface.sounds.sd')
+        sound_patcher = patch('term_timer.interface.sounds.sd', create=True)
         sound_patcher.start()
         self.addCleanup(sound_patcher.stop)
 
